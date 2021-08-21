d32ea308ae277d808b2e16461d2c851648c773a6
Yago Ananias <yago_souza92@yahoo.com.br>
2021-08-21 11:00:45 -0300

    Add Ex3

---
+++ Aula2/A2Ex3/src/a2ex3/A2Ex3.java
@@ -0,0 +1,16 @@
+package a2ex3;
+
+public class A2Ex3 {
+
+    public static void main(String[] args) {
+        
+        Data dataTeste = new Data();
+        
+        dataTeste.setDia(21);
+        dataTeste.setMes(8);
+        dataTeste.setAno(2021);
+        
+        dataTeste.exibeData();
+    }
+    
+}
