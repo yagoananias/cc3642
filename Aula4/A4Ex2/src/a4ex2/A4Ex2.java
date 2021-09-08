a5211be748ea398e8c10ba7bbe1c74d7af22b25d
Yago Ananias <yago_souza92@yahoo.com.br>
2021-09-08 09:34:24 -0300

    Add A4Ex2

---
+++ Aula4/A4Ex2/src/a4ex2/A4Ex2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a4ex2;
+
+/**
+ *
+ * @author Yago
+ */
+public class A4Ex2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
