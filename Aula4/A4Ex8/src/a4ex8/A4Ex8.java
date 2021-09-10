188092069026267f98a98dcd19b3499778286f67
Yago Ananias <yago_souza92@yahoo.com.br>
2021-09-10 15:02:48 -0300

    Finished A4Ex8

--- Aula4/A4Ex8/src/a4ex8/A4Ex8.java
+++ Aula4/A4Ex8/src/a4ex8/A4Ex8.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package a4ex8;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -11,11 +9,76 @@ package a4ex8;
  */
 public class A4Ex8 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Scanner input = new Scanner(System.in);
+        ArrayList<Pessoa> contatos = new ArrayList<>();
+        boolean mainLoop = true;
+        String escolha;
+
+        while (mainLoop) {
+
+            System.out.println("");
+            System.out.println("======= Menu Agenda =======");
+            System.out.print("n) Criar novo contato. \n");
+            System.out.print("d) Deletar um contato.\n");
+            System.out.print("p) Exibir agenda.\n");
+            System.out.print("q.) Sair.\n");
+            System.out.print("\nDigite sua opção: ");
+
+            escolha = input.nextLine();
+
+            switch (escolha) {
+                case "n":
+                    System.out.println("Adicionando pessoa na agenda...");
+                    System.out.println("Digite o nome e o telefone ");
+
+                    contatos.add(new Pessoa(String.valueOf(input.nextLine()),
+                            String.valueOf(input.nextLine())));
+
+                    for (Pessoa p : contatos) {
+                        System.out.println("Meus contatos: ");
+                        System.out.println(p);
+                    }
+
+                    break;
+
+                case "d":
+                    System.out.println("Excluindo pessoa da agenda...");
+                    System.out.println("Digite o nome ");
+
+                    for (int i = 0; i < contatos.size(); i++) {
+                        String escolhido;
+                        Scanner deletar = new Scanner(System.in);
+                        escolhido = (String)deletar.nextLine();
+                        if (escolhido.equals(contatos.get(i).getNome())) {
+                            contatos.remove(i);
+                            System.out.println("Contato removido!");
+                        }
+                    }
+                    break;
+
+                case "p":
+                    
+                    for (Pessoa p : contatos) {
+                        System.out.println("Meus contatos: ");
+                        System.out.println(p);
+                    }
+                    break;
+
+                case "q":
+                    System.out.println("Saindo da agenda...");
+                    System.exit(0);
+                    break;
+                    
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+
+            }
+
+        }
+
     }
-    
+
 }
--- Aula4/A4Ex8/src/a4ex8/Pessoa.java
+++ Aula4/A4Ex8/src/a4ex8/Pessoa.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package a4ex8;
 
 /**
@@ -10,5 +5,47 @@ package a4ex8;
  * @author Yago
  */
 public class Pessoa {
+    
+    private String nome;
+    private String telefone;
+    private int id;
+    private static int nextId = 1;
+
+    public Pessoa(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.id = nextId++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "nome=" + nome + ", telefone=" + telefone + ", id=" + id + '}';
+    }
+    
+    
     
 }
