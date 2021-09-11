bbc330b9108e4c1c209949a1d6fdcd0bfafcf14b
Yago Ananias <yago_souza92@yahoo.com.br>
2021-09-11 06:36:47 -0300

    Add A5Ex1

---
+++ Aula5/A5Ex1/src/a5ex1/A5Ex1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a5ex1;
+
+/**
+ *
+ * @author Yago
+ */
+public class A5Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
