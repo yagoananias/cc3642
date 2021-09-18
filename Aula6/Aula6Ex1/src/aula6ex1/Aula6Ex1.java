85bc324ef2f8b3d0964945fd4ae1e6d5e5a987ea
Yago Ananias <yago_souza92@yahoo.com.br>
2021-09-18 19:59:22 -0300

    Add Aula6Ex1

---
+++ Aula6/Aula6Ex1/src/aula6ex1/Aula6Ex1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula6ex1;
+
+/**
+ *
+ * @author Yago
+ */
+public class Aula6Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Quadrado q = new Quadrado();
+        System.out.println(q.square(5));
+        System.out.println(q.square(2.5));
+    }
+    
+}
---
+++ Aula6/Aula6Ex1/src/aula6ex1/Quadrado.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula6ex1;
+
+/**
+ *
+ * @author Yago
+ */
+public class Quadrado {
+    public int square(int x) {
+        return x*x;
+    }
+    
+    public double square(double x) {
+        return x*x;
+    }
+    
+}
