7627ad4dad3abd909948b11ea47d62b21807b891
Yago Ananias <yago_souza92@yahoo.com.br>
2021-09-21 05:50:16 -0300

    Add A6Ex3

---
+++ Aula6/Aula6Ex3/src/aula6ex3/Advogado.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula6ex3;
+
+/**
+ *
+ * @author Yago
+ */
+public class Advogado extends Agente {
+    
+    private String nome;
+    private boolean modo_agente;
+    private String profissao;
+    private String OAB;
+
+    @Override
+    void apresentacao() {
+        System.out.println("");
+    }
+    
+}
