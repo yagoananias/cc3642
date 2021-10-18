f7bf653e8a5e24e236bb565a9bf49f1c3c122fc2
Yago Ananias <yago_souza92@yahoo.com.br>
2021-10-18 08:53:42 -0300

    A10Ex2

--- Aula10/A10Ex2/src/a10ex2/A10Ex2.java
+++ Aula10/A10Ex2/src/a10ex2/A10Ex2.java
@@ -5,6 +5,8 @@
  */
 package a10ex2;
 
+import view.Janela;
+
 /**
  *
  * @author Yago
@@ -15,7 +17,8 @@ public class A10Ex2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Janela j = new Janela();
+        j.setVisible(true);
     }
     
 }
