d10f91196822239eb3bf7dbf74e459419dd5dce0
Yago Ananias <yago_souza92@yahoo.com.br>
2021-10-22 21:52:33 -0300

    Add Calculadora

---
+++ Aula10/Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import view.Calc;
+
+/**
+ *
+ * @author Yago
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calc janela = new Calc();
+        janela.setVisible(true);
+    }
+    
+}
