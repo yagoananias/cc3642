2f3dcf93fa079b19f3aac4a159db56e879956e18
Yago Ananias <yago_souza92@yahoo.com.br>
2021-10-30 10:58:55 -0300

    Add Aula11 Ex1

---
+++ Aula11/A11Ex1/src/a11ex1/A11Ex1.java
@@ -0,0 +1,19 @@
+package a11ex1;
+
+import view.JanelaPizzaria;
+
+/**
+ *
+ * @author Yago
+ */
+public class A11Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JanelaPizzaria janela = new JanelaPizzaria();
+        janela.setVisible(true);
+    }
+    
+}
