c6b39af11d977e642decb95a601f9a3ec0bc3514
Yago Ananias <yago_souza92@yahoo.com.br>
2021-11-23 19:54:12 -0300

    Add Simulado

---
+++ Simulado/poupanca/src/poupanca/Poupanca.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poupanca;
+
+import view.TelaPoupanca;
+
+/**
+ *
+ * @author Yago
+ */
+public class Poupanca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaPoupanca j = new TelaPoupanca();
+        j.setVisible(true);
+    }
+    
+}
